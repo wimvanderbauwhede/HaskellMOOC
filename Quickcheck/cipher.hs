ab484ed599f20e7bbed86791fa118f029a9d6736
Jeremy Singer <Jeremy.Singer@glasgow.ac.uk>
2016-09-12 12:45:46 +0100

    updatign cipher src codde

--- Quickcheck/cipher.hs
+++ Quickcheck/cipher.hs
@@ -1,19 +1,42 @@
+-- Caesar's Cipher example
+-- Jeremy.Singer@glasgow.ac.uk
+
 import Data.Char
-import Test.QuickCheck
 
 
 -- is this a letter to be ciphered?
 shouldcipher :: Char -> Bool
 shouldcipher c = isLetter(c) && isAscii(c)
 
+
 -- enciphers single char at a time
 cipherchar :: Int -> Char -> Char
 cipherchar shift c
- | shouldcipher c =  chr(ord(c) + shift)
+ | shouldcipher c = chr(ord(c)+shift)
  | otherwise      = c
 
 
--- should we wrap around the alphabet
+
+-- encipher a whole string
+cipher :: Int -> [Char] -> [Char]
+cipher shift plaintext = map (bettercipherchar shift) plaintext
+
+
+
+
+
+
+decipher :: Int -> [Char] -> [Char]
+decipher shift ciphertext = cipher (-shift) ciphertext
+
+
+
+
+
+
+
+
+-- should we wrap around the alphabet?
 wraparound shift c 
  | isLower(c) && ord(c)+shift > ord 'z' = True
  | isUpper(c) && ord(c)+shift > ord 'Z' = True
@@ -24,15 +47,7 @@ bettercipherchar :: Int -> Char -> Char
 bettercipherchar shift c
  | shouldcipher c =  chr(ord(c) + adjustedshift)
  | otherwise      = c
- where adjustedshift = let shift' = shift `mod` 26 in if (wraparound shift' c) then shift'-26 else shift'
-
-
--- encipher a whole string
-cipher :: Int -> [Char] -> [Char]
-cipher shift plaintext = map (bettercipherchar shift) plaintext
-
-
-decipher :: Int -> [Char] -> [Char]
-decipher = \x y-> (cipher (-x) (y))
-
-
+ where adjustedshift = let shift' = shift `mod` 26
+                       in if (wraparound shift' c)
+                          then shift'-26
+                          else shift'
