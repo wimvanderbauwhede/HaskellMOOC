ab37d3971586553b6f55c31a4badefc9bfa19a64
Jeremy Singer <Jeremy.Singer@glasgow.ac.uk>
2016-09-16 11:53:12 +0100

    added comments before posting

--- Starman/starman.hs
+++ Starman/starman.hs
@@ -6,12 +6,14 @@
 
 
 check :: String -> String -> Char -> (Bool, String)
+-- check whether a single char is in the mystery word
 check word display c
   = (c `elem` word, [if x==c
           then c
           else y | (x,y) <- zip word display])
 
 turn :: String -> String -> Int -> IO ()
+-- single turn for user
 turn word display n =
   do if n==0
        then putStrLn "You lose"
@@ -20,6 +22,7 @@ turn word display n =
               else mkguess word display n
 
 mkguess :: String -> String -> Int -> IO ()
+-- user inputs a single char (first on the line)
 mkguess word display n =
   do putStrLn (display ++ "  " ++ take n (repeat '*'))
      putStr "  Enter your guess: "
@@ -28,6 +31,10 @@ mkguess word display n =
      let n' = if correct then n else n-1
      turn word display' n'
 
+-- notice how turn and mkguess have the same signatures,
+-- and are mutually recursive. Is this elegant?
+
 starman :: String -> Int -> IO ()
+-- top-level function. Usage: starman "WORD" NUM_TURNS
 starman word n = turn word ['-' | x <- word] n
 
